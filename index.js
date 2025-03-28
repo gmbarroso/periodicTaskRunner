1b629452414a3d65f7a0d33b1d9748c77818bc62
gmbarroso <barroso.guilherme@gmail.com>
2025-03-27 21:22:19 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const schedule = require('node-schedule');
+const logger = require('./utils/logger');
+const { cleanRevokedTokens, cleanBookings, cleanInactiveBookings } = require('./tasks/cleanTasks');
+
+schedule.scheduleJob('59 23 * * 5', async () => {
+  logger.info('Starting cleanup of the revoked_token table...');
+  await cleanRevokedTokens();
+});
+
+schedule.scheduleJob('0 0 1 1,4,7,10 *', async () => {
+  logger.info('Starting cleanup of the booking table...');
+  await cleanBookings();
+});
+
+schedule.scheduleJob('0 0 1 1,7 *', async () => {
+  logger.info('Starting cleanup of inactive booking records...');
+  await cleanInactiveBookings();
+});
+
+logger.info('Tasks successfully scheduled.');

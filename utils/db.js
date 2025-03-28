4b192eaddf5a98c2bd1b56367675a36bdd16e84b
gmbarroso <barroso.guilherme@gmail.com>
2025-03-27 21:23:10 -0300

    add config files

---
+++ utils/db.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+const logger = require('./logger');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+});
+
+pool.on('connect', () => logger.info('Database connection established.'));
+pool.on('error', (err) => logger.error('Error in database connection:', err));
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};

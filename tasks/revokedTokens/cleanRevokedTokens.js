0af1ff5cbe8a08a0bd12ab2d3ca3086f22650eca
gmbarroso <barroso.guilherme@gmail.com>
2026-02-27 21:25:27 -0300

    refactor: Extract SQL query to a constant and improve error handling in cleanRevokedTokens function

--- tasks/revokedTokens/cleanRevokedTokens.js
+++ tasks/revokedTokens/cleanRevokedTokens.js
@@ -2,18 +2,19 @@ const db = require('../../utils/db');
 const handleTaskError = require('../../utils/errorHandler');
 const logger = require('../../utils/logger');
 
+const DELETE_EXPIRED_REVOKED_TOKEN_SQL = `
+  DELETE FROM revoked_token
+  WHERE "expirationDate" <= NOW()
+`;
+
 async function cleanRevokedTokens() {
   try {
-    const query = `
-      DELETE FROM revoked_token
-      WHERE "expirationDate" <= NOW()
-    `;
-    const result = await db.query(query);
+    const result = await db.query(DELETE_EXPIRED_REVOKED_TOKEN_SQL);
     logger.info(`revoked_token table cleaned successfully. ${result.rowCount} rows deleted.`);
     return result.rowCount;
   } catch (error) {
-    handleTaskError('cleanRevokedTokens', error, 'DELETE FROM revoked_token WHERE "expirationDate" <= NOW()');
-    return 0;
+    handleTaskError('cleanRevokedTokens', error, DELETE_EXPIRED_REVOKED_TOKEN_SQL);
+    return null;
   }
 }
 
